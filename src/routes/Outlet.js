b3bbc7a644abf82d171b24bb9093a26ae0a572a5
helvin159 <helvin159@gmail.com>
2024-02-12 15:15:16 -0500

    scaffold project

---
+++ src/routes/Outlet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Outlet as Layout } from 'react-router-dom';
+
+const Outlet = () => {
+	return (
+		<main>
+			<Layout />
+		</main>
+	);
+};
+
+export default Outlet;
