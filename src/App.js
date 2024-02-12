aa6b082a0f2803ff9457899359689a8695138922
helvin159 <helvin159@gmail.com>
2024-02-12 15:52:17 -0500

    scaffold

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './routes/Home';
 import Outlet from './routes/Outlet';
+import Reservation from './routes/Reservation';
+import Pricing from './routes/Pricing';
+import About from './routes/About';
+import Contact from './routes/Contact';
 
 function App() {
 	return (
 		<Routes>
 			<Route path='/' element={<Outlet />}>
 				<Route index element={<Home />} />
+				<Route path='/reservation' element={<Reservation />} />
+				<Route path='/pricing' element={<Pricing />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/contact' element={<Contact />} />
 			</Route>
 		</Routes>
 	);
